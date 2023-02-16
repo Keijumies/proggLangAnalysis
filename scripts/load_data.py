669fd3810490124572b74a52c470b790f9f7ac1f
Keijumies <125501964+Keijumies@users.noreply.github.com>
2023-02-16 14:26:45 +0100

    Update load_data.py
    make a big list of all programming languages mentioned with for loop and split(;)

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -10,3 +10,14 @@ import numpy as np
 import matplotlib.pyplot as plt   
 
 lingo_data = pd.read_csv("C:/Users/Otto/Documents/GitHub/proggLangAnalysis/data/data.csv")
+big_list = []
+
+# dataframe[key][index]
+# split each string with split(;), save into list and append to big list
+
+for i, row in lingo_data.iterrows():
+    x = lingo_data['LanguagesWorkedWith'][i].split(';')
+    big_list.append(x)
+    print(i)
+
+print(big_list)
