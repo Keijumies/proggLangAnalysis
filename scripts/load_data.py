0231722cbe798c3a3bf826d73ec2e7b86d2ce518
Keijumies <125501964+Keijumies@users.noreply.github.com>
2023-02-16 13:00:27 +0100

    InitialSetup
    File sructure and load data

---
+++ scripts/load_data.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 16 12:35:00 2023
+
+@author: Otto
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt   
+
+lingo_data = pd.read_csv("C:/Users/Otto/Documents/GitHub/proggLangAnalysis/data/data.csv")
